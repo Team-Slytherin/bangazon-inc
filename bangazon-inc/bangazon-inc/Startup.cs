859c493199e00027647f38754bc16e989c079638
Justin Leggett <justin@codingrookie.com>
2017-05-08 19:03:32 -0500

    initial push

---
+++ bangazon-inc/bangazon-inc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bangazon_inc.Startup))]
+namespace bangazon_inc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
