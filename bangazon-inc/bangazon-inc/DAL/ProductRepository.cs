1d3cbc9855a0c4030d8f60d2970bc35fb3208a68
Dustin Reed <dmreedy1@gmail.com>
2017-05-09 19:09:00 -0500

    added Dapper and StructureMap

---
+++ bangazon-inc/bangazon-inc/DAL/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using bangazon_inc.Interfaces;
+using bangazon_inc.Models;
+using Dapper;
+
+namespace bangazon_inc.DAL
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public ProductRepository(IDbConnection connection)
+        {
+            _dbConnection = connection;
+        }
+        public void AddProduct(Product newProduct)
+        {
+            const string sql = @"INSERT into BangazonInc.dbo.Product
+                        (ProductName,
+                         ProductPrice)
+                        VALUES
+                        (@ProductName,
+                         @ProductPrice);";
+            _dbConnection.Execute(sql, newProduct);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var sql = $@"SELECT * FROM BangazonInc.dbo.Product;";
+            return _dbConnection.Query<Product>(sql).ToList();
+        }
+
+        public Product GetOneProduct(int productId)
+        {
+            var sql = $@"SELECT *
+                        FROM BangazonInc.dbo.Product
+                        WHERE ProductId = {productId};";
+            var result = _dbConnection.Query<Product>(sql).ToList();
+            return result.FirstOrDefault();
+        }
+
+        public void UpdateProduct(Product productToUpdate)
+        {
+            const string sql = @"UPDATE Product
+                        SET ProductName to @ProductName
+                            ProductPrice to @ProductPrice
+                        WHERE ProductId = @ProductId";
+            _dbConnection.Execute(sql, new { productToUpdate });
+        }
+    }
+}
---
+++ bangazon-inc/bangazon-inc/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bangazon_inc.Models;
+
+namespace bangazon_inc.Interfaces
+{
+    public interface IProductRepository
+    {
+        void AddProduct(Product newProduct);
+        Product GetOneProduct(int productId);
+        IEnumerable<Product> GetAllProducts();
+        void UpdateProduct(Product productToUpdate);
+    }
+}
