31f9a44c5a45bacae951c1c0e3fbed802784167b
Justin Leggett <justin@codingrookie.com>
2017-05-16 14:27:44 -0500

    attempting to remove automatic scaffolding and add manually

--- bangazon-inc/bangazon-inc/Controllers/ProductsController.cs
+++ bangazon-inc/bangazon-inc/Controllers/ProductsController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using bangazon_inc.DAL;
+using bangazon_inc.Migrations;
 using bangazon_inc.Models;
 
 namespace bangazon_inc.Controllers
@@ -14,129 +12,34 @@ namespace bangazon_inc.Controllers
     public class ProductsController : Controller
     {
         private AppContext db = new AppContext();
-
         // GET: Products
         public ActionResult Index()
         {
-            return View(db.Products.ToList());
-        }
-
-        // GET: Products/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            var categories = db.Categories.ToList();
+            var customers = db.Customers.ToList();
+            var viewModel = new CreateProductViewModel
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
+                Categories = categories.Select(x => new SelectListItem { Text = x.CategoryName, Value = x.CategoryId.ToString() }).ToList(),
+                Customers = customers.Select(x => new SelectListItem { Text = x.CustomerFirstName + "" + x.CustomerLastName, Value = x.CustomerId.ToString() }).ToList(),
+                ProductViewId = 2
+            };
+
+            return View(viewModel);
         }
 
-        // GET: Products/Category/5
-        public ActionResult Category(int? id)
-        {
-
-            ViewBag.Products = db.Products.Where(x => x.Category.CategoryId == id).ToList();
-            return View();
-        }
-
-
-        // GET: Products/Create
-        public ActionResult Create()
-        {
-            Product product = new Product();
-            product.Customer = db.Customers.Find(2);
-            product.Category = db.Categories.Find(2);
-            return View(product);
-        }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Price,Category,Description,Image,Customer")] Product product)
+        public ActionResult CreateProduct(CreateProductViewModel product)
         {
-            if (ModelState.IsValid)
+            var res = new Product
             {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                
+            };
 
-            return View(product);
-        }
+            //res.DuckIAmEating = _duckRepository.Get(reservation.DuckId);
 
-        // GET: Products/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
+            //_reservationRepository.Save(res);
 
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Price,Category,Description,Image")] Product product)
-        {
-            product.Customer = db.Customers.Find(2);
-            product.ProductId = 1;
-            db.Entry(product).State = EntityState.Modified;
-            db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        // GET: Products/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-
-            Product product = db.Products.Find(id);
-            product.Customer = db.Customers.Find(2);
-
-
-            db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
---
+++ bangazon-inc/bangazon-inc/Models/CreateProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bangazon_inc.Models
+{
+    public class CreateProductViewModel
+    {
+        //[DisplayName("Number of People Eating Duck")]
+        //public int NumberOfPeople { get; set; }
+        [DisplayName("Which ProductViewId")]
+        [Key]
+        public int ProductViewId { get; set; }
+
+        public virtual Product Product { get; set; }
+        [DisplayName("Which Category")]
+        public List<SelectListItem> Categories { get; set; }
+        [DisplayName("Which Customer")]
+        public List<SelectListItem> Customers { get; set; }
+
+    }
+}
